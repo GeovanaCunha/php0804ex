579b3ae63bf7b9e414e76f95b7f5c31a46766193
GeovanaCunha <geovanahcunha1109@gmail.com>
2025-04-11 14:28:46 -0300

    arrumando

--- cedulas de dinheiro/calcular.php
+++ cedulas de dinheiro/calcular.php
@@ -3,12 +3,12 @@ $valor = $_POST['valor'];
 
 $notas = [100, 50, 20, 10, 5, 2, 1];
 
-echo "<h2>Nota necessaria para compor R$$valor,00:</h2>";
+echo "<h2>Nota necessaria para compor R$ $valor,00:</h2>";
 
 foreach ($notas as $nota) {
     $quantidade = intdiv($valor, $nota);
     if ($quantidade > 0) {
-        echo "$quantidade notas de R$$nota,00<br>";
+        echo "$quantidade notas de R$ $nota,00<br>";
         $valor = $valor % $nota;
     }
 }
