c299ad630de7fab025f6d204416d92bcb12c9dab
EmillyChristinny <emillychristinnyal@gmail.com>
2025-04-11 08:11:09 -0300

    criando index

---
+++ Subtração Aleatória/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Subtração Aleatória</title>
+</head>
+<body>
+    <h2>Digite um número:</h2>
+    <form action="calcular.php" method="post">
+        <input type="number" name="numero" required>
+        <button type="submit">Enviar</button>
+    </form>
+</body>
+</html>
