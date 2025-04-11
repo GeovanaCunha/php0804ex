c5eab39c81b23eb622cc189ed959151d23bce2df
EmillyChristinny <emillychristinnyal@gmail.com>
2025-04-11 08:15:36 -0300

    mudando usuario

---
+++ Adivinhação Automática/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Adivinhando o número</title>
+</head>
+<body>
+    <h2>Clique para gerar até acertar o número base</h2>
+    <form action="calcular.php" method="post">
+        <button type="submit">Começar</button>
+    </form>
+</body>
+</html>
