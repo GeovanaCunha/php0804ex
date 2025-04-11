d6b0ab4053e6f8c2f6c13aa4f07a085a848624f1
GeovanaCunha <geovanahcunha1109@gmail.com>
2025-04-11 15:27:13 -0300

    Update index.php

--- desafio/index.php
+++ desafio/index.php
@@ -3,7 +3,7 @@ $sorteado = rand(1, 100);
 ?>
 
 <h2>Adivinhe o número</h2>
-<form action="../resultado/verifica.php" method="post">
+<form action="http://localhost/php0804ex-1/desafio/resultado.php" method="post">
   <input type="number" name="palpite" required>
   <input type="hidden" name="sorteado" value="<?php echo $sorteado; ?>">
   <input type="hidden" name="tentativas" value="1">
