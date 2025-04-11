82bd42140aded93e3acfbce403ecc8764c056ee7
EmillyChristinny <emillychristinnyal@gmail.com>
2025-04-11 14:46:47 -0300

    Update calcular.php

--- cedulas de dinheiro/calcular.php
+++ cedulas de dinheiro/calcular.php
@@ -1,10 +1,21 @@
 <?php
 
-$notas = [100, 50, 20, 10, 5, 2, 1];
-if ($notas)
-{
-    $quantidade = intdiv ($valor, $nota);
-    $valor %= $nota;
-    echo "R$ $nota,00: $quantidade /n";
+$valor = intval($_POST["valor"]);
+
+$cedulas = [100, 50, 20, 10, 5, 2, 1];
+
+foreach ($cedulas as $cedula) {
+
+$qtd = intdiv($valor, $cedula);
+
+if ($qtd > 0) {
+
+echo "$qtd nota(s) de R$$cedula, 00<br>";
+
+$valor %= $cedula;
+
 }
+
+}
+
 ?>
