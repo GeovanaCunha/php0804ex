e4e456bb15c5f17537a965938f59bb3749d3b1dc
GeovanaCunha <geovanahcunha1109@gmail.com>
2025-04-11 15:22:36 -0300

    arrumando

--- desafio/resultado.php
+++ desafio/resultado.php
@@ -15,7 +15,7 @@ if ($palpite == $sorteado) {
   echo "<h2>Errou!</h2>";
   echo "<p>Dica: o número é " . ($palpite < $sorteado ? "maior" : "menor") . ".</p>";
 
-  echo "<form action='verifica.php' method='post'>
+  echo "<form action='http://localhost/php0804ex-1/desafio/resultado.php' method='post'>
           <input type='number' name='palpite' required>
           <input type='hidden' name='sorteado' value='$sorteado'>
           <input type='hidden' name='tentativas' value='$tentativas'>
